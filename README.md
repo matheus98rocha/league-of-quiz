6a9f078855d33a05acaabe5bfecc18ca428a2e68
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-01-27 00:26:25 -0300

    Conclusão da segunda aula

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-Material desenvolvido durate a semana de imersão React da Alura;
 
 Tecnologias:
 React.js;
 Next.js
+
+
+<h2 align="center">
+League of quiz
+</h2>
+<h4 align="center">
+  Material desenvolvido durate a semana de imersão React da Alura;
+</h4>
+
+
+## Installing
+
+```shell
+git clone https://github.com/matheus98rocha/imersao-react-alura
+
+```
+
--- leagueofquiz/pages/index.js
+++ leagueofquiz/pages/index.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget/index';
 import Footer from '../src/components/Footer/index';
 import GitHubCorner from '../src/components/GitHubCorner/index';
 import QuizBackground from '../src/components/QuizBackground/index';
+import QuizLogo from '../src/components/QuizLogo/index';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -24,20 +29,42 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>League Of Quiz</title>
+      </Head>
       <QuizContainer>
+
+        <QuizLogo />
 
         <Widget>
           <Widget.Header>
             <h1>League Of Legends</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Em breve disponivel</p>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              router.push(`/quiz?$name=${name}`);
+            }}
+            >
+              <input
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
 
         </Widget>
-
 
         <Widget>
           <Widget.Header>
@@ -51,5 +78,5 @@ export default function Home() {
       </QuizContainer>
       <GitHubCorner />
     </QuizBackground>
-  )
+  );
 }
